325f5c346d067eb367e0258d8deaf82f2d9678b5
Priyanka <pri.kashyap5@gmail.com>
2026-05-02 21:34:15 +0530

    chore: add logging using Lombok

--- src/main/java/com/npsystemlabs/urlshortener/UrlShortenerApplication.java
+++ src/main/java/com/npsystemlabs/urlshortener/UrlShortenerApplication.java
@@ -1,11 +1,14 @@
 package com.npsystemlabs.urlshortener;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class UrlShortenerApplication {
     public static void main(String[] args) {
         SpringApplication.run(UrlShortenerApplication.class, args);
+        log.info("Url Shortener Application is running");
     }
 }
